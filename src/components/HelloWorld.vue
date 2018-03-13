a0825c8c340365d678c08bc1a836c43e9ceddec9
Wei Ren <weiren@berkeley.edu>
2018-03-13 02:20:25 -0700

    blockly

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,30 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div id="blocklyDiv" style="height: 480px; width: 600px;"></div>
   </div>
 </template>
 
 <script>
+import * as Blockly from 'google-blockly/blockly_accessible_uncompressed'
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted () {
+    this.programming()
+  },
+  methods: {
+    programming () {
+      var toolbox = '<xml>'
+      toolbox += '  <block type="controls_if"></block>'
+      toolbox += '  <block type="controls_whileUntil"></block>'
+      toolbox += '</xml>'
+      var workspace = Blockly.inject('blocklyDiv', {toolbox: toolbox})
+      console.log(workspace)
     }
   }
 }
